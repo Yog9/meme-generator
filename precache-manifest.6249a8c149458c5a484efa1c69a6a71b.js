96b97446cdadbff2f32b38bf8be58f79541606a2
Yogita Verma <veryogita@gmail.com>
2019-03-04 15:06:16 +0530

    Updates

---
+++ precache-manifest.6249a8c149458c5a484efa1c69a6a71b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "345d16a9bd0e43a266f9",
+    "url": "/meme-generator/static/js/runtime~main.345d16a9.js"
+  },
+  {
+    "revision": "3523d666494496cb11b8",
+    "url": "/meme-generator/static/js/main.3523d666.chunk.js"
+  },
+  {
+    "revision": "556f4301741ceea6db01",
+    "url": "/meme-generator/static/js/2.556f4301.chunk.js"
+  },
+  {
+    "revision": "3523d666494496cb11b8",
+    "url": "/meme-generator/static/css/main.1d2f5886.chunk.css"
+  },
+  {
+    "revision": "972a65e74c907ced960e1486079bfcaa",
+    "url": "/meme-generator/index.html"
+  }
+];
